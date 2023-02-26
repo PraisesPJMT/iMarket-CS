f440cc53addedb6494940c599ccc4b28896cecb9
PraisesPJMT <praisesmusa@outlook.com>
2023-02-26 23:36:01 +0100

    Add `RegistrationForm` component

---
+++ src/utilities/Types.ts
@@ -0,0 +1,12 @@
+// Reg. Form State Type
+export interface RegFormStateType {
+  firstName: string;
+  middlename: string;
+  surname: string;
+  email: string;
+  password: string;
+  phoneNumber: string;
+  confirmPwd: string;
+  address: string;
+  role: string;
+}
